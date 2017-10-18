b8a7e21c3b5bb657ed5479d265f09854374b7073
Kwang Yul Seo <kseo@kodebox.io>
2017-10-18 23:07:52 +0900

    Rewrite MetaCoin using Zeppelin's StandardToken

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -1,36 +1,15 @@
 pragma solidity ^0.4.4;
 
-import "./ConvertLib.sol";
+import "zeppelin/contracts/token/StandardToken.sol";
 
-// This is just a simple example of a coin-like contract.
-// It is not standards compatible and cannot be expected to talk to other
-// coin/token contracts. If you want to create a standards-compliant
-// token, see: https://github.com/ConsenSys/Tokens. Cheers!
-
-contract MetaCoin {
-	mapping (address => uint) balances;
-
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+contract MetaCoin is StandardToken {
+	string public name ="CodeChainToken";
+	string public symbol = "CCT";
+	uint public decimals = 18;
+	uint public INITIAL_SUPPLY = 10000;
 
 	function MetaCoin() {
-		balances[tx.origin] = 10000;
-	}
-
-	function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-		if (balances[msg.sender] < amount) {
-			return false;
-		}
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		Transfer(msg.sender, receiver, amount);
-		return true;
-	}
-
-	function getBalanceInEth(address addr) returns(uint) {
-		return ConvertLib.convert(getBalance(addr),2);
-	}
-
-	function getBalance(address addr) returns(uint) {
-		return balances[addr];
+		totalSupply = INITIAL_SUPPLY;
+		balances[msg.sender] = INITIAL_SUPPLY;
 	}
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
+module.exports = function (deployer) {
   deployer.deploy(MetaCoin);
 };
